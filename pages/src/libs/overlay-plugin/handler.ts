70cc57ea423dbf70f4e7fd761d7a796378d2a57c
qitana <53415856+qitana@users.noreply.github.com>
2024-08-25 08:29:22 +0000

    chore: Remove unused code and interfaces in overlay-plugin/handler.ts

--- pages/src/libs/overlay-plugin/handler.ts
+++ pages/src/libs/overlay-plugin/handler.ts
@@ -9,58 +9,6 @@
 // -----------------------------------------------------------------------------
 
 
-// Member names taken from OverlayPlugin's MiniParse.cs
-// Types taken from FFXIV parser plugin
-export interface PluginCombatantState {
-  CurrentWorldID?: number;
-  WorldID?: number;
-  WorldName?: string;
-  BNpcID?: number;
-  BNpcNameID?: number;
-  PartyType?: number;
-  ID?: number;
-  OwnerID?: number;
-  WeaponId?: number;
-  Type?: number;
-  Job?: number;
-  Level?: number;
-  Name?: string;
-  CurrentHP: number;
-  MaxHP: number;
-  CurrentMP: number;
-  MaxMP: number;
-  PosX: number;
-  PosY: number;
-  PosZ: number;
-  Heading: number;
-
-  MonsterType?: number;
-  Status?: number;
-  ModelStatus?: number;
-  AggressionStatus?: number;
-  TargetID?: number;
-  IsTargetable?: boolean;
-  Radius?: number;
-  Distance?: string;
-  EffectiveDistance?: string;
-  NPCTargetID?: number;
-  CurrentGP?: number;
-  MaxGP?: number;
-  CurrentCP?: number;
-  MaxCP?: number;
-  PCTargetID?: number;
-  IsCasting1?: number;
-  IsCasting2?: number;
-  CastBuffID?: number;
-  CastTargetID?: number;
-  CastGroundTargetX?: number;
-  CastGroundTargetY?: number;
-  CastGroundTargetZ?: number;
-  CastDurationCurrent?: number;
-  CastDurationMax?: number;
-  TransformationId?: number;
-}
-
 type BroadcastHandler = (msg: {
   call: 'broadcast';
   source: string;
@@ -72,13 +20,6 @@ type SubscribeHandler = (msg: {
   events: string[];
 }) => void;
 
-type GetCombatantsHandler = (msg: {
-  call: 'getCombatants';
-  ids?: number[];
-  names?: string[];
-  props?: string[];
-}) => { combatants: PluginCombatantState[] };
-
 type OpenWebsiteWithWSHandler = (msg: {
   call: 'openWebsiteWithWS';
   url: string;
@@ -87,7 +28,6 @@ type OpenWebsiteWithWSHandler = (msg: {
 export type OverlayHandlerAll = {
   'broadcast': BroadcastHandler;
   'subscribe': SubscribeHandler;
-  'getCombatants': GetCombatantsHandler;
   'openWebsiteWithWS': OpenWebsiteWithWSHandler;
 };
 
